d6ebd1896cbcdee0b8feb39bcb3a4c1d1973cbf5
danfh <danfh9@gmail.com>
2024-06-28 17:49:54 +0300

    feat: created pdf plugin
    Created a plugin that embeds pdfs into an iframe so
    they can be viewed and used with the watch plugin.
    
    Author: Daniel F.H.

---
+++ packages/pdf/src/index.ts
@@ -0,0 +1,24 @@
+import type { Plugin } from 'previewed';
+
+export default function pdf({
+    filetypes = ['pdf'],
+}: { filetypes?: string[] } = {}) {
+    const plugin: Plugin = (file, filePath) => {
+        if (!filetypes.every((filetype) => !filePath.startsWith(filetype))) {
+            return file;
+        }
+        return Buffer.from(`
+        <!DOCTYPE html>
+        <html>
+            <body>
+                <iframe
+                    src="data:application/pdf;base64,${file.toString('base64')}"
+                    style="width: 100%; height: 100%; position: absolute;"
+                    frameborder="0"
+                >
+            </body>
+        </html>    
+        `);
+    };
+    return plugin;
+}
